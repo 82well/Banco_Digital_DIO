330a2a4972fd3225591b75c2e6701abee15fa6e1
82well <nylook2@hotmail.com>
2022-12-20 21:35:37 -0300

    Atualizando implementos

--- BancoDigitalDio/src/Conta.java
+++ BancoDigitalDio/src/Conta.java
@@ -6,7 +6,23 @@ public class Conta implements iConta{
 	private int numero;
 	private double saldo;
 	
+	@Override
+	public void sacar(double valor) {
+		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public void depositar(double valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void transferir(double valor, Conta contaDestio) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	public int getAgencia() {
 		return agencia;
@@ -19,5 +35,7 @@ public class Conta implements iConta{
 	public double getSaldo() {
 		return saldo;
 	}
+
+
 	
 }
