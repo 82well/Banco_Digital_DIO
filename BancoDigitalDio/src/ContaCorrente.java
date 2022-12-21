e7ccc0fdd40c5928df5dd2937e193047be96395e
82well <nylook2@hotmail.com>
2022-12-20 22:06:20 -0300

    Modificadores

--- BancoDigitalDio/src/ContaCorrente.java
+++ BancoDigitalDio/src/ContaCorrente.java
@@ -1,12 +1,5 @@
 
 public class ContaCorrente extends Conta {
 
-	
-	private static int SEQUENCIAL = 1;
 
-	public ContaCorrente() {
-		super.agencia = Conta.AGENCIA_PADRAO;
-		super.numero = SEQUENCIAL++;
-	}
-	
 }
